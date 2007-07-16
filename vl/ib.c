a674b4b316056b492a1948237aec5b8b713739ce
Brian Fulkerson <bfulkers@cs.ucla.edu>
2007-07-16 03:26:57 -0700

    Add initial versions of agglomerative information bottleneck merging

---
+++ vl/ib.c
@@ -0,0 +1,234 @@
+#include "ib.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h> /* DBL_MAX */
+#include <math.h>
+
+#define BETA_MAX DBL_MAX
+typedef struct _VlIB
+{
+    vl_node   * nodes; 
+    vl_double * beta;
+    vl_double * bidx;
+    vl_node nnodes;
+
+    vl_node * which;  
+    vl_node nwhich;
+
+    vl_prob * Pic;
+    vl_prob * Pi;
+    vl_node nrows;
+    vl_node ncols;
+} VlIB;
+
+void vl_ib_normalizeP(vl_prob * P, vl_node nrows, vl_node ncols)
+{
+    vl_node i;
+    vl_prob sum = 0;
+    for(i=0; i<nrows*ncols; i++)
+        sum += P[i];
+    for(i=0; i<nrows*ncols; i++)
+        P[i] /= sum;
+}
+
+vl_node * vl_ib_new_nodelist(vl_node nrows)
+{
+    vl_node * nodelist = malloc(sizeof(vl_node)*nrows);
+    vl_node n;
+    for(n=0; n<nrows; n++)
+        nodelist[n] = n;
+
+    return nodelist;
+}
+
+vl_prob * vl_ib_new_Pi(vl_prob * Pic, vl_node nrows, vl_node ncols)
+{
+    vl_prob * Pi = malloc(sizeof(vl_prob)*nrows);
+    vl_node r,c;
+    for(r=0; r<nrows; r++)
+    {
+        vl_prob sum = 0;
+        for(c=0; c<ncols; c++)
+            sum += Pic[r*ncols+c];
+        Pi[r] = sum;
+    }
+    return Pi;
+}
+
+void vl_ib_min_beta(VlIB * ib, vl_node * besti, vl_node * bestj, vl_double * minbeta)
+{
+    *minbeta = ib->beta[0];
+    *besti   = 0;
+    *bestj   = ib->bidx[0];
+
+    vl_node i;
+    for(i=0; i<ib->nnodes; i++)
+        if(*minbeta < ib->beta[i])
+        {
+            *minbeta = ib->beta[i];
+            *besti = i;
+            *bestj = ib->bidx[i];
+        }
+}
+
+void vl_ib_merge_nodes(VlIB * ib, vl_node i, vl_node j, vl_node new)
+{
+    vl_node lastnode = ib->nnodes-1;
+    vl_node c;
+
+    /* merge i and j */
+    for(c=0; c<ib->ncols; c++)    /* Pic */
+        ib->Pic[i*ib->ncols + c] += ib->Pic[j*ib->ncols + c];
+    ib->Pi[i] += ib->Pi[j]; /* Pi */
+    ib->beta[i] = BETA_MAX; /* beta */
+    ib->bidx[i] = 0; /* bidx */
+    ib->nodes[i] = new; /* nodes */
+
+    /* copy lastnode into j */
+    for(c=0; c<ib->ncols; c++)    /* Pic */
+        ib->Pic[j*ib->ncols + c] = ib->Pic[lastnode*ib->ncols + c];
+    ib->Pi[j] = ib->Pi[lastnode]; /* Pi */
+    ib->beta[j] = ib->beta[lastnode]; /* beta */
+    ib->bidx[j] = ib->bidx[lastnode]; /* bidx */
+    ib->nodes[j] = ib->nodes[lastnode]; /* nodes */
+
+    /* delete a node */
+    ib->nnodes--;
+}
+
+#define PLOGP(x) (x)*log((x))
+void vl_ib_update_beta(VlIB * ib)
+{
+    vl_node i;
+    vl_prob * Pi  = ib->Pi;
+    vl_prob * Pic = ib->Pic;
+
+    for(i=0; i<ib->nwhich; i++)
+    {
+        vl_node a = ib->which[i];
+        vl_node b;
+        for(b=0; b<ib->nnodes; b++)
+        {
+            if(a==b) continue;
+
+            vl_double C1 = 0;
+
+            if (Pi [a] == 0 || Pi [b] == 0) continue;
+
+            C1 = 
+/*                + ib->tmp [a] 
+                  + ib->tmp [b] */
+                - PLOGP ((Pi[a] + Pi[b])) ;
+
+            vl_node c;
+            for (c = 0 ; c < ib->ncols; ++ c) {
+                vl_double Pac = Pic [a*ib->ncols + c] ;
+                vl_double Pbc = Pic [b*ib->ncols + c] ;
+
+                if(Pac != 0)
+                    C1 += -Pac*log(Pac/Pi[a]); /* tmpa */
+                if(Pbc != 0)
+                    C1 += -Pbc*log(Pbc/Pi[b]); /* tmpb */
+
+                if (Pac == 0 && Pbc == 0) continue;
+                C1 += PLOGP ((Pac + Pbc)) ;
+            }
+
+            /* TODO: Does it matter if we pick min beta or max C1 - Beta*C2? */
+            vl_double beta = -C1;
+
+            if (beta < ib->beta[a])
+            {
+                ib->beta[a] = beta;
+                ib->bidx[a] = b;
+            }
+            if (beta < ib->beta[b])
+            {
+                ib->beta[b] = beta;
+                ib->bidx[b] = a;
+            }
+        }
+    }
+}
+
+VlIB * vl_ib_new_ib(vl_prob * Pic, vl_node nrows, vl_node ncols)
+{
+    VlIB * ib = malloc(sizeof(VlIB));
+    ib->Pic = Pic;
+    ib->nrows = nrows;
+    ib->ncols = ncols;
+
+    vl_ib_normalizeP(ib->Pic, ib->nrows, ib->ncols);
+    ib->Pi = vl_ib_new_Pi(ib->Pic, ib->nrows, ib->ncols);
+
+    /* nodelist contains all the remaining nodes. This also has to be modified
+     * after a merge, but order doesn't matter (as long as Pi and Pic agree) */
+    vl_node nnodes = nrows;
+    ib->nodes = vl_ib_new_nodelist(nnodes);
+    ib->beta   = malloc(sizeof(vl_prob)*nnodes);
+    vl_node i;
+    for(i=0; i<nnodes; i++)
+        ib->beta[i] = BETA_MAX;
+    
+    /* Have to manipulate the which list (delete) */
+    /* order of which doesn't matter */
+    ib->nwhich = nrows;
+    ib->which  = vl_ib_new_nodelist(ib->nwhich);
+
+    return ib;
+}
+
+void vl_ib_delete_ib(VlIB * ib)
+{
+    free(ib->nodes);
+    ib->nodes = 0;
+    free(ib->beta);
+    ib->beta = 0;
+    free(ib->which);
+    ib->which = 0;
+    free(ib->Pi);
+    ib->Pi = 0;
+    free(ib);
+}
+
+vl_node * vl_ib(vl_prob * Pic, vl_node nrows, vl_node ncols)
+{
+    vl_node * parents = calloc(nrows*2-1, sizeof(vl_node));
+
+    VlIB * ib = vl_ib_new_ib(Pic, nrows, ncols);
+
+    vl_node i, besti, bestj, newnode;
+    vl_double minbeta;
+    /* Initially which = all */
+
+    /* For each merge */
+    for(i=0; i<nrows-1; i++)
+    {
+        /* Update those rows which need to be updated */
+        vl_ib_update_beta(ib);
+
+        /* Find best merge */
+        vl_ib_min_beta(ib, &besti, &bestj, &minbeta);
+
+        /* Add the parent pointers for the new node */
+        newnode = nrows+i;
+        parents[ib->nodes[besti]] = newnode;
+        parents[ib->nodes[bestj]] = newnode;
+
+        /* Merge the nodes which produced the minimum beta */
+        vl_ib_merge_nodes(ib, besti, bestj, newnode);
+        vl_ib_update_which();
+        /* Remove node from:
+         *  o which
+         *  o nodelist 
+         *  o Pic
+         *  o Pi
+         **/
+    }
+
+    vl_ib_delete_ib(ib);
+    ib = 0;
+
+    return parents;
+} 
---
+++ vl/ib.h
@@ -0,0 +1,9 @@
+#ifndef _IB_H_
+#define _IB_H_
+
+#include "generic.h"
+
+typedef vl_double vl_prob ;
+typedef vl_uint32 vl_node ;
+
+#endif
