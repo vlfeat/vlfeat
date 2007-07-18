d7c8d936310f302f51b2ac423029b52773323fdf
Brian Fulkerson <bfulkers@cs.ucla.edu>
2007-07-18 04:44:39 -0700

    Added IB mex file

---
+++ toolbox/ib.mex.c
@@ -0,0 +1,64 @@
+/** @file     ib.mex.c
+ ** @author   Brian Fulkerson
+ ** @brief    IB MEX driver
+ ** @internal
+ **/
+
+#include "mexutils.h"
+#include <vl/mathop.h>
+#include <vl/ib.h>
+#include <assert.h>
+
+
+/** @brief MEX entry point */
+void
+mexFunction(int nout, mxArray *out[], 
+            int nin, const mxArray *in[])
+{
+  enum {IN_PIC=0} ;
+  enum {OUT_PARENTS=0} ;
+
+  vl_prob * P = mxGetPr(in[IN_PIC]);
+  vl_node rows = mxGetM(in[IN_PIC]);
+  vl_node cols = mxGetN(in[IN_PIC]);
+  vl_prob * Pic = malloc(sizeof(vl_prob)*rows*cols);
+  int r,c;
+  fprintf(stderr, "transposing\n");
+  for(r=0; r<rows; r++)
+      for(c=0; c<cols; c++)
+          Pic[r*cols+c] = P[c*rows+r];
+
+  /*
+  fprintf(stderr, "dumping Pic\n");
+  for(r=0; r<rows; r++)
+  {
+      for(c=0; c<cols; c++)
+      {
+          fprintf(stderr, "%f ", Pic[r*cols+c]);
+      }
+      fprintf(stderr, "\n");
+  }
+  */
+
+  vl_node * parents = vl_ib(Pic, rows, cols);
+  int dims[2];
+  /* Create an empty array */
+  dims[0] = 2*rows-1;
+  dims[1] = 1;
+  out[OUT_PARENTS] = mxCreateNumericArray(2, dims, mxUINT32_CLASS, mxREAL);
+  vl_node * mexparents = (vl_node *)mxGetData(out[OUT_PARENTS]);
+
+  int n;
+  for(n=0; n<2*rows-1; n++)
+      if(parents[n] == 2*rows)
+          mexparents[n] = 0;
+      else
+          mexparents[n] = parents[n]+1; /* parents + 1 for matlab indexing */
+
+  /* Fill it with our stuff
+  dims[0] = rows*2-1;
+  dims[1] = 1;
+  mxSetDimensions(out[OUT_PARENTS], dims, 2);
+  mxSetData(out[OUT_PARENTS], parents);
+  */
+}
