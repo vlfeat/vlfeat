53db5aaf8008758c309a4ec97d84855eeda4ddba
Andrea Vedaldi <vedaldi@Cs-176-219.CS.UCLA.EDU>
2007-11-03 17:40:21 -0700

    added imdown.m

---
+++ toolbox/imdown.m
@@ -0,0 +1,29 @@
+function J = imdown(I,method)
+% IMDOWN  Downsample image 
+%   J = IMDOWN(I,'sample') downsamples the image I by half by
+%   discarding each other pixel.
+%   
+%   IMDOWN(I,'avg') downsmples by averaging groups of four pixels.
+%
+%   The image is always converted to double format.
+%   
+%   See also IMUP().
+
+% AUTORIGHTS
+
+if nargin < 2
+  method = 'sample' ;
+end
+
+switch method
+  case 'sample'
+    J = I(1:2:end,1:2:end,:) ;
+    
+  case 'avg'
+    J = ...
+        I(1:2:end-1,1:2:end-1,:) + ...
+        I(2:2:end,1:2:end-1,:) + ...
+        I(1:2:end-1,2:2:end,:) + ...
+        I(2:2:end,2:2:end,:) ;
+    J = J / 4 ;
+end
