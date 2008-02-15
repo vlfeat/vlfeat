1030182bf76f83ef9e412619d1c846b93e19fd88
Andrea Vedaldi <vedaldi@morio.cs.ucla.edu>
2008-02-14 18:53:32 -0800

    new help_vlfeat.m

---
+++ toolbox/help_vlfeat.m
@@ -0,0 +1,23 @@
+% HELP_VLFEAT  VLFeat MATLAB Tooblox
+%   The VLFeat MATLAB toolbox provides implementation of common
+%   computer vision algorithms (SIFT, MSER, AIB, KMEANS, ...) and
+%   other useful MATLAB functions.
+%
+%   * Use SIFT() to compute the Scale-Invariant Feature Transform.
+%
+%   * Use MSER() to compute the Maximally-Stable Extremal Regions.
+%
+%   * Use IKMEANS() and HIKMEANS() to compute integer K-means (IKM)
+%     and hierarchical IKM. Here integer denotes the fact that the
+%     implementations are optimized for vector of integers. 
+%
+%   See http://vision.ucla.edu/vlfeat
+
+% AUTORIGHTS
+% Copyright 2007 (c) Andrea Vedaldi and Brian Fulkerson
+% 
+% This file is part of VLFeat, available in the terms of the GNU
+% General Public License version 2.
+
+help help_vlfeat.m
+
