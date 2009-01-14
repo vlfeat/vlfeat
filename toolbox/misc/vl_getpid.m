96d3707857195efc1359d18d8525014ed51eff6a
Andrea Vedaldi <vedaldi@gmail.com>
2009-01-14 14:14:16 +0000

    vl_getpid.{c|m}: new MEX function to retrieve MATLAB process PID

---
+++ toolbox/misc/vl_getpid.m
@@ -0,0 +1,7 @@
+% VL_GETPID  Get MATLAB process ID
+%   VL_GETPID() returns MATLAB process ID. 
+%
+%   Algorithm:: VL_GETPID() returns the result of getpid() (UNIX) or
+%     GetCurrentProcessID() (Windows) system functions.
+%
+%   Author:: Andrea Vedaldi
