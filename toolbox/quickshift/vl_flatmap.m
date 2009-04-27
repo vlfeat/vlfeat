c660811ef8269a0ca1c7d3c1730f96a625ace14a
Brian Fulkerson <bfulkers@cs.ucla.edu>
2009-04-27 13:54:30 -0700

    quickshift: vl_flatmap documentation fix

--- toolbox/quickshift/vl_flatmap.m
+++ toolbox/quickshift/vl_flatmap.m
@@ -1,8 +1,8 @@
-function [map, C] = vl_flatmap(map)
-% VL_FLATMAP
+% VL_FLATMAP Flatten a tree, assigning the label of the root to each node
 %   [LABELS CLUSTERS] = VL_FLATMAP(MAP) labels each tree of the forest contained
 %   in MAP. LABELS contains the linear index of the root node in MAP, CLUSTERS
 %   instead contains a label between 1 and the number of clusters.
+function [map, C] = vl_flatmap(map)
 
 % follow the parents list to the root nodes (where nothing changes)
 while 1
