1d68e14884916bf32cb99a702c2b259b1a0775bf
Andrea Vedaldi <vedaldi@sefer.(none)>
2009-06-15 06:13:45 -0700

    vl_roc.m: comments

--- toolbox/plotop/vl_roc.m
+++ toolbox/plotop/vl_roc.m
@@ -1,9 +1,9 @@
 function [tp,tn,info] = vl_roc(y, score, varargin)
-% VL_ROC Compute the ROC curve
-%  [TP,TN] = VL_ROC(Y, SCORE) computes the VL_ROC curve of the specified
+% VL_ROC Compute ROC curve
+%  [TP,TN] = VL_ROC(Y, SCORE) computes the ROC curve for the specified
 %  data. Y are the ground thruth labels (+1 or -1) and SCORE is the
-%  discriminant score associated to the data by a classifier (higher
-%  scores correspond to positive labels).
+%  discriminant score assigned to them by a classifier (higher scores
+%  correspond to positive labels).
 %
 %  [TP,TN] are the true positive and true negative rates for
 %  incereasing values of the decision threshold.
@@ -20,50 +20,47 @@ function [tp,tn,info] = vl_roc(y, score, varargin)
 %
 %  VL_ROC(...) plots the VL_ROC diagram in the current axis.
 %
-%  About the VL_ROC curve::
-%    Consider a classifier that predicts as positive al lables whose
-%    SCORE is not smaller than a threshold. The VL_ROC curve represents
-%    the performance of such classifier as the threshold r is
-%    varied. Denote:
+%  About the ROC curve::
+%    Consider a classifier that predicts as positive all lables Y
+%    whose SCORE is not smaller than a threshold R. The ROC curve
+%    represents the performance of such classifier as the threshold R
+%    is varied. Let:
 %
-%      P  = num of positive samples
-%      N  = num of negative samples
-%      TP = num of samples that are correctly classified as positive
-%      TN = num of samples that are correctly classified as negative
-%      FP = num of samples that are incorrectly classified as positive
-%      FN = num of samples that are incorrectly classified as negative
+%      #P   = num of positive samples
+%      #N   = num of negative samples
+%      #TP = num of samples that are correctly classified as positive
+%      #TN = num of samples that are correctly classified as negative
+%      #FP = num of samples that are incorrectly classified as positive
+%      #FN = num of samples that are incorrectly classified as negative
 %
 %    Consider also the rates:
 %
-%                TP_ = TP / P,      FN_ = FN / P,
-%                TN_ = TN / N,      FP_ = FP / N.
+%                TP = #TP / #P,      FN = #FN / #P,
+%                TN = #TN / #N,      FP = #FP / #N.
 %
 %    Notice that:
 %
-%                 P = TP  + FN ,    N = TN  + FP,
-%                 1 = TP_ + FN_,    1 = TN_ + FP_.
+%                 P = #TP + #FN ,    N = #TN + #FP,
+%                 1 = TP + FN,       1 = TN + FP.
 %
-%    The VL_ROC curve is the parametric curve (TP_, TN_) obtained
-%    as the classifier threshold is changed.
+%    The ROC curve is the parametric curve (TP, TN) obtained as the
+%    classifier threshold R is varied.
 %
-%    The VL_ROC curve is contained in the square with vertices (0,0)
-%    The (average) VL_ROC curve of a random classifier is a line which
+%    The ROC curve is contained in the square with vertices (0,0) The
+%    (average) ROC curve of a random classifier is a line which
 %    connects (1,0) and (0,1).
 % 
-%    The VL_ROC curve is independent of the prior probability of positive
-%    PPOS and negative labels PNEG. For instance, the empirical
-%    expected error (01-risk) is
+%    The ROC curve is independent of the prior probability of the
+%    labels Y (i.e. of #P/(#P+#N) and #N/(#P+#N)).
 %
-%        ERR = FP_ PPOS + FN_ PNEG,   PPOS = P/(P+N), 
-%                                     PNEG = N/(P+N). 
+%    An OPERATING POINT is a point on the ROC curve, corresponding to
+%    a certain threshold R. Each operating point corresponds to
+%    minimizing the empirical 01 error of the classifier for given
+%    prior probabilty of the labels Y. VL_ROC() computes the following
+%    operating points:
 %
-%    An OPERATING POINT is a point on the VL_ROC curve, corresponding to
-%    a certain threshold. Each operating point minimizes the empirical
-%    error for certain label priors PPOS and PPNEG.  VL_ROC() computes
-%    the following operating points:
-%
-%     Natural operating point:: Assumes PPOS = P/(P+N).
-%     Uniform operating point:: Assumes PPOS = 1/2.
+%     Natural operating point:: Assumes P[Y=+1] = P/(P+N).
+%     Uniform operating point:: Assumes P[Y=+1] = 1/2.
 %
 %  See also:: VL_HELP().
 
