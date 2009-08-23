d083bb47ed69906bb5d3cc38888f6d9b60436f90
Andrea Vedaldi <vedaldi@gmail.com>
2009-08-23 12:55:59 +0200

    vl_numder.m: improves the documentation and adds support for an arbitrary array as input of the function to be derived

--- toolbox/misc/vl_numder.m
+++ toolbox/misc/vl_numder.m
@@ -1,23 +1,24 @@
 function d = vl_numder(func, x, varargin)
 % VL_NUMDER  Numerical derivative
-%   D = VL_NUMDER(FUNC, X) computes the numerical derivative of 
-%   the function FUNC at point X.
+%   D = VL_NUMDER(FUNC, X) computes the numerical derivative of the
+%   function FUNC at point X. X is a real array and is passed as first
+%   argument of FUNC.
 %
-%   D = VL_NUMDER(FUNC, X, ARG1, ARG2, ...) allow to pass extra
-%   parameters to the function FUNC.
+%   D = VL_NUMDER(FUNC, X, ARG1, ARG2, ...) passes ARG1, ARG2, ...  as
+%   additional arguments to the function FUNC.
 %
 %   See also:: VL_NUMDER2(), VL_HELP().
 
 % TODO: uniform sacaling of axis is not a good idea
 
-dx=1e-7 ;
-
-N=length(x) ;
+dx = 1e-7 ;
+N  = numel(x) ;
 fx = feval(func, x, varargin{:}) ;
-d=zeros(length(fx(:)),N) ;
+d  = zeros(length(fx(:)),N) ;
+
 for n=1:N
-  e=zeros(size(x)) ; e(n)=1 ;
+  e   = zeros(size(x)) ; e(n) = 1 ;
   fxn = feval(func, x+dx*e, varargin{:}) ;
-  di = (fxn-fx)/dx ;
-  d(:,n) = di(:) ; 
+  di  = (fxn - fx) / dx ;
+  d(:,n) = di(:) ;
 end
