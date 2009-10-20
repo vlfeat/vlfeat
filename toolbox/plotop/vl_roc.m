6828786d827af6629726983b4140b6c2027d015c
Andrea Vedaldi <vedaldi@gmail.com>
2009-10-20 23:55:56 +0100

    vl_roc.m: fixes a bug causing unpredictable output when mixed column/row vectors were passed as input. Fixes whitespaces.

--- toolbox/plotop/vl_roc.m
+++ toolbox/plotop/vl_roc.m
@@ -14,7 +14,7 @@ function [tp,tn,info] = vl_roc(y, score, varargin)
 %  INFO.EER::       Equal error rate.
 %  INFO.AUC::       Area under the VL_ROC (AUC).
 %  INFO.UR::        Uniform prior best op point rate.
-%  INFO.UT::        Uniform prior best op point threhsold. 
+%  INFO.UT::        Uniform prior best op point threhsold.
 %  INFO.NR::        Natural prior best op point rate.
 %  INFO.NT::        Natural prior best op point threshold.
 %
@@ -49,7 +49,7 @@ function [tp,tn,info] = vl_roc(y, score, varargin)
 %    The ROC curve is contained in the square with vertices (0,0) The
 %    (average) ROC curve of a random classifier is a line which
 %    connects (1,0) and (0,1).
-% 
+%
 %    The ROC curve is independent of the prior probability of the
 %    labels Y (i.e. of #P/(#P+#N) and #N/(#P+#N)).
 %
@@ -65,37 +65,35 @@ function [tp,tn,info] = vl_roc(y, score, varargin)
 %   Options:
 %
 %   Plot::
-%     'truenegative' plots the true positives vs. the true negatives.
-%     'falsenegative' plots the true positives vs. the false 
+%     'TrueNegative' plots the true positives vs. the true negatives.
+%     'FalseNegative' plots the true positives vs. the false
 %     negatives.
 %
 %  See also:: VL_HELP().
 
 % AUTORIGHTS
 % Copyright 2007 (c) Andrea Vedaldi and Brian Fulkerson
-% 
+%
 % This file is part of VLFeat, available in the terms of the GNU
 % General Public License version 2.
 
 plotenable = (nargout == 0);
 plotstyle = 'truenegative';
 for k=1:2:length(varargin)
-  switch lower(varargin{k}) 
+  switch lower(varargin{k})
     case 'plot'
       plotenable = 1;
-      plotstyle = varargin{k+1} ;
+      plotstyle = lower(varargin{k+1}) ;
     otherwise
-      error(['Uknown option ''', varargin{k}, '''.']) ;
+      error(['Unknown option ''', varargin{k}, '''.']) ;
   end
 end
 
-y = y(:);
-score = score(:); % vectors
+% make row vectors
+y = y(:)' ;
+score = score(:)' ;
 
 [score, perm] = sort(score) ;
-
-% important for the flipping below
-perm = perm(:)' ;
 
 Np = sum(y==+1) ;
 Nn = sum(y==-1) ;
@@ -109,14 +107,11 @@ end
 % fliplr(perm) move the high scored ones to the beginning
 tp = cumsum( y(fliplr(perm)) == +1 ) / (Np + eps) ;
 tp = fliplr(tp) ;
-tp = [tp; 0] ;
+tp = [tp, 0] ;
 
 % true negative rate
 tn = cumsum(y(perm) == -1) / (Nn + eps)  ;
-tn = [0; tn] ;
-
-% voc style
-%[tp, tn] = roc_pascal05(y, score) ;
+tn = [0, tn] ;
 
 % --------------------------------------------------------------------
 %                                                      Additional info
@@ -129,22 +124,22 @@ if nargout > 0 | nargout == 0
   i2 = min(find(tn >= tp)) ;
 %  eer = (tp(i) + tn(i)) / 2 ;
   eer = max(tn(i1), tp(i2)) ;
-  
+
   % uniform prior and natural prior operating points
-  [drop, upoint] = max(tp + tn) ;	
-  [drop, npoint] = max(tp .* Np + tn .* Nn) ;	
-  
+  [drop, upoint] = max(tp + tn) ;
+  [drop, npoint] = max(tp .* Np + tn .* Nn) ;
+
   % uniform prior and natural prior operationg points rates and thresholds
   ur = tp(upoint) * 1/2  + tn(upoint) * 1/2 ;
   nr = tp(npoint) * Np/N + tn(npoint) * Nn/N ;
-  
-  score_ = [-inf; score] ;
+
+  score_ = [-inf, score] ;
   ut = score_(upoint) ;
   nt = score_(npoint) ;
-  
+
   % area
   area = sum((tp(1:end-1)+tp(2:end)) .* diff(tn))/2 ;
-  
+
   % save
   info.eer  = eer ;
   info.auc  = area ;
@@ -160,74 +155,51 @@ end
 
 % plot?
 if plotenable
-		
+
 	cla ; hold on ;
 	plot(tn,tp,'linewidth',2) ;
 
   switch plotstyle
-  case 'truenegative'
-    line(eer             * [0 1 1], ...
-         eer             * [1 1 0], ...
-         'color','r', 'linestyle', '--','linewidth', 1) ;
-    line(tn(upoint) * [0 1 1], ...
-         tp(upoint) * [1 1 0], ...
-         'color','g', 'linestyle', '--', 'linewidth', 1) ;
-    line(tn(npoint) * [0 1 1], ...
-         tp(npoint) * [1 1 0], ...
-         'color','b', 'linestyle', '--', 'linewidth', 1) ;
-    line([0 1], [1 0], 'color','b', 'linestyle', ':', 'linewidth', 2) ;
-    line([0 1], [0 1], 'color','y', 'linestyle', '-', 'linewidth', 1) ;
-    
-    xlim([0 1]) ; xlabel('true negative rate') ;
-    ylim([0 1]) ; ylabel('true positve rate') ;
-  case 'falsenegative'
-    line(1 - eer             * [0 1 1], ...
-             eer             * [1 1 0], ...
-         'color','r', 'linestyle', '--','linewidth', 1) ;
-    line(1 - tn(upoint) * [0 1 1], ...
-             tp(upoint) * [1 1 0], ...
-         'color','g', 'linestyle', '--', 'linewidth', 1) ;
-    line(1 - tn(npoint) * [0 1 1], ...
-             tp(npoint) * [1 1 0], ...
-         'color','b', 'linestyle', '--', 'linewidth', 1) ;
-    line([1 0], [1 0], 'color','b', 'linestyle', ':', 'linewidth', 2) ;
-    line([1 0], [0 1], 'color','y', 'linestyle', '-', 'linewidth', 1) ;
-    
-    xlim([0 1]) ; xlabel('false negative rate') ;
-    ylim([0 1]) ; ylabel('true positve rate') ;    
-  otherwise
-    error(sprintf('Unrecognized plotting style: %s', plotstyle));
+
+    case 'truenegative'
+      line(eer             * [0 1 1], ...
+           eer             * [1 1 0], ...
+           'color','r', 'linestyle', '--','linewidth', 1) ;
+      line(tn(upoint) * [0 1 1], ...
+           tp(upoint) * [1 1 0], ...
+           'color','g', 'linestyle', '--', 'linewidth', 1) ;
+      line(tn(npoint) * [0 1 1], ...
+           tp(npoint) * [1 1 0], ...
+           'color','b', 'linestyle', '--', 'linewidth', 1) ;
+      line([0 1], [1 0], 'color','b', 'linestyle', ':', 'linewidth', 2) ;
+      line([0 1], [0 1], 'color','y', 'linestyle', '-', 'linewidth', 1) ;
+
+      xlim([0 1]) ; xlabel('true negative rate') ;
+      ylim([0 1]) ; ylabel('true positve rate') ;
+
+    case 'falsenegative'
+      line(1 - eer             * [0 1 1], ...
+           eer             * [1 1 0], ...
+           'color','r', 'linestyle', '--','linewidth', 1) ;
+      line(1 - tn(upoint) * [0 1 1], ...
+           tp(upoint) * [1 1 0], ...
+           'color','g', 'linestyle', '--', 'linewidth', 1) ;
+      line(1 - tn(npoint) * [0 1 1], ...
+           tp(npoint) * [1 1 0], ...
+           'color','b', 'linestyle', '--', 'linewidth', 1) ;
+      line([1 0], [1 0], 'color','b', 'linestyle', ':', 'linewidth', 2) ;
+      line([1 0], [0 1], 'color','y', 'linestyle', '-', 'linewidth', 1) ;
+
+      xlim([0 1]) ; xlabel('false negative rate') ;
+      ylim([0 1]) ; ylabel('true positve rate') ;
+    otherwise
+      error(sprintf('Unrecognized plotting style: %s', plotstyle));
   end
   axis square ;
-	title(sprintf('VL_ROC (AUC = %.3g)', area)) ;
+	title(sprintf('VL_ROC (AUC = %.3g)', area), 'interpreter', 'none') ;
 	legend('ROC', ...
 				 sprintf('eer     %.3g %%', 100 * eer), ...
 				 sprintf('op unif %.3g %%', 100 * ur), ...
 				 sprintf('op nat  %.3g %%', 100 * nr), ...
-				 'random order', 'Location', 'SouthWest') ;    
+				 'random order', 'Location', 'SouthWest') ;
 end
-
-% --------------------------------------------------------------------
-function [tp,tn] = roc_pascal05(y, score)
-% --------------------------------------------------------------------
-
-pres = y > 0 ;
-confidence = score ;
-n = length(score) ;
-
-% VOC code starts here
-rp=randperm(n); % sort equal confidences randomly
-pres=pres(rp);
-confidence=confidence(rp);
-
-np=sum(pres);
-nn=n-np;
-
-[sc,si]=sort(-confidence);
-sp=pres(si);
-roc.tp=cumsum(sp)/np;
-roc.fp=cumsum(~sp)/nn;
-% VOC code stops here
-
-tp = fliplr(roc.tp) ;
-tn = fliplr(1 - roc.fp) ;
