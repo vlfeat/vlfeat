a991e088bd282a6f36bd932f9d30c9acb97c1c27
Andrea Vedaldi <vedaldi@gmail.com>
2010-01-15 11:56:06 +0000

    host.h: completes definition of printf formatting stings for builtin datatypes

--- vl/host.h
+++ vl/host.h
@@ -13,6 +13,132 @@
 #ifndef VL_HOST_H
 #define VL_HOST_H
 
+/** ------------------------------------------------------------------
+ ** @name C preprocessor helper macros
+ ** @{ */
+
+/** @brief Convert the argument to a string
+ **
+ ** @param x value to be stringified.
+ **
+ ** This macro stringifies the argument @a x by means of the
+ ** <code>#</code> prerpocessor operator.
+ **
+ ** The standard C preprocessor does not prescan arguments which are
+ ** stringified, so
+ **
+ ** @code
+ ** #define A B
+ ** char const * str = VL_STRINGIFY(A) ;
+ ** @endcode
+ **
+ ** initializes <code>str</code> with a pointer to the string
+ ** <code>"A"</code>, which mihgt be unexpected. To fix this issue,
+ ** you can use ::VL_XSTRINGIFY.
+ **
+ ** @sa ::VL_XSTRINGIFY
+ **/
+
+#define VL_STRINGIFY(x) # x
+
+/** @brief Expand and then convert the argument to a string
+ **
+ ** @param x value to be macro-expanded and converted.
+ **
+ ** This macro macro-expands the argument @a x and stringifies the
+ ** result of the expansion. For instance
+ **
+ ** @code
+ ** #define A B
+ ** char const * str = VL_STRINGIFY(A) ;
+ ** @endcode
+ **
+ ** initializes <code>str</code> with a pointer to the string
+ ** <code>"B"</code>.
+ **
+ ** @sa ::VL_STRINGIFY
+ **/
+#define VL_XSTRINGIFY(x) VL_STRINGIFY(x)
+
+/** @brief Concatenate two arguments into a lexical unit
+ **
+ ** @param x first argument to be concatenated.
+ ** @param y second argument to be concatenated.
+ **
+ ** This macro concatenates its arguments into a single lexical unit
+ ** by means of the <code>##</code> preprocessor operator. Notice that
+ ** arguments concatenated by <code>##</code> are not pre-expanded by
+ ** the C preprocessor. To macro-expand the arguments and then
+ ** concatenate them,use ::VL_XCAT.
+ **
+ ** @see ::VL_XCAT
+ **/
+#define VL_CAT(x,y) x ## y
+
+/** @brief Expand and then concatenate two arguments into a lexical unit
+ **
+ ** @param x first argument to be concatenated.
+ ** @param y second argument to be concatenated.
+ **
+ ** This macro is the same as ::VL_CAT, except that the arguments are
+ ** macro expanded before being concatenated.
+ **
+ ** @see ::VL_CAT
+ **/
+#define VL_XCAT(x,y) VL_CAT(x,y)
+
+/** @brief Expand and then concatenate three arguments into a lexical unit
+ **
+ ** @param x first argument to be concatenated.
+ ** @param y second argument to be concatenated.
+ ** @param z third argument to be concatenated.
+ **
+ ** This macro is the same as ::VL_XCAT, except that the arguments are three.
+ **
+ ** @see ::VL_XCAT
+ **/
+#define VL_XCAT3(x,y,z) VL_XCAT(VL_XCAT(x,y),z)
+
+/** @brief Expand and then concatenate four arguments into a lexical unit
+ **
+ ** @param x first argument to be concatenated.
+ ** @param y second argument to be concatenated.
+ ** @param z third argument to be concatenated.
+ ** @param u fourth argument to be concatenated.
+ **
+ ** This macro is the same as ::VL_XCAT, except that the arguments are four.
+ **
+ ** @see ::VL_XCAT
+ **/
+#define VL_XCAT4(x,y,z,u) VL_XCAT(VL_XCAT3(x,y,z),u)
+
+/** @brief Expand and then concatenate five arguments into a lexical unit
+ **
+ ** @param x first argument to be concatenated.
+ ** @param y second argument to be concatenated.
+ ** @param z third argument to be concatenated.
+ ** @param u fourth argument to be concatenated.
+ ** @param v fifth argument to be concatenated.
+ **
+ ** This macro is the same as ::VL_XCAT, except that the arguments are five.
+ **
+ ** @see ::VL_XCAT
+ **/
+#define VL_XCAT5(x,y,z,u,v) VL_XCAT(VL_XCAT4(x,y,z,u),v)
+
+/** @} */
+
+/** @brief Convert a boolean to "yes" or "no" strings
+ ** @param x boolean to convert.
+ ** A pointer to either the string "yes" (if @a x is true)
+ ** or the string "no".
+ ** @par Example
+ ** @code
+ ** VL_PRINTF("Is x true? %s.", VL_YESNO(x))
+ ** @endcode
+ **/
+#define VL_YESNO(x) ((x)?"yes":"no")
+
 /*
  The following macros identify the host OS, architecture and compiler.
  They are derived from http://predef.sourceforge.net/
@@ -243,22 +369,28 @@ typedef vl_uint32           vl_uindex ;
  ** @name Printing the atomic data types
  ** @{ */
 
+/* Lengths only: */
 
 /** @def VL_FL_INT64
- ** @brief @c prinf length flag for ::vl_int64 and ::vl_uint64.
+ ** @brief @c printf length flag for ::vl_int64 and ::vl_uint64.
  **/
 
 /** @def VL_FL_INT32
- ** @brief @c prinf length flag for ::vl_int32 and ::vl_uint32.
+ ** @brief @c printf length flag for ::vl_int32 and ::vl_uint32.
  **/
 
 /** @def VL_FL_INT16
- ** @brief @c prinf length flag for ::vl_int16 and ::vl_uint16.
+ ** @brief @c printf length flag for ::vl_int16 and ::vl_uint16.
  **/
 
 /** @def VL_FL_INT8
- ** @brief @c prinf length flag for ::vl_int8 and ::vl_uint8.
+ ** @brief @c printf length flag for ::vl_int8 and ::vl_uint8.
  **/
+
+/** @def VL_FL_INDEX
+ ** @biref @c printf length flag for ::vl_index and ::vl_uindex
+ **/
+
 #ifdef VL_COMPILER_MSC
 #define VL_FL_INT64  "I64"
 #else
@@ -267,6 +399,43 @@ typedef vl_uint32           vl_uindex ;
 #define VL_FL_INT32  ""
 #define VL_FL_INT16  "h"
 #define VL_FL_INT8   "hh"
+
+#if defined(VL_COMPILER_LP64) || defined(VL_COMPILER_LLP64)
+#define VL_FL_INDEX VL_FL_INT64
+#endif
+
+#if defined(VL_COMPILER_ILP32)
+#define VL_FL_INDEX VL_FL_INT32
+#endif
+
+/* Formats (but not conversions!): */
+
+/** @def VL_FMT_SIZE
+ ** @brief @c printf flag for ::vl_size
+ **/
+
+/** @def VL_FMT_INDEX
+ ** @brief @c printf flag for ::vl_index
+ **/
+
+/** @def VL_FMT_UINDEX
+ ** @brief @c printf flag for ::vl_uindex
+ **/
+
+/** @def VL_FMT_INTPTR
+ ** @brief @c printf flag for ::vl_intptr
+ **/
+
+/** @def VL_FMT_UINTPTR
+ ** @brief @c printf flag for ::vl_uintptr
+ **/
+
+#define VL_FMT_INDEX   VL_FL_INDEX "d"
+#define VL_FMT_INTPTR  VL_FMT_INDEX
+#define VL_FMT_UINDEX  VL_FL_INDEX "u"
+#define VL_FMT_SIZE    VL_FMT_UINDEX
+#define VL_FMT_UINTPTR VL_FMT_UINDEX
+
 /** @} */
 
 /** ------------------------------------------------------------------
@@ -385,106 +554,6 @@ vl_swap_host_big_endianness_2 (void *dst, void* src)
     dst_ [1] = src_ [0] ;
 #endif
 }
-
-
-/** ------------------------------------------------------------------
- ** @name C preprocessor helper macros
- ** @{ */
-
-/** @brief Convert the argument to a string
- **
- ** @param x value to be stringified.
- **
- ** This macro stringifies the argument @a x by means of the
- ** <code>#</code> prerpocessor operator.
- **
- ** The standard C preprocessor does not prescan arguments which are
- ** stringified, so
- **
- ** @code
- ** #define A B
- ** char const * str = VL_STRINGIFY(A) ;
- ** @endcode
- **
- ** initializes <code>str</code> with a pointer to the string
- ** <code>"A"</code>, which mihgt be unexpected. To fix this issue,
- ** you can use ::VL_XSTRINGIFY.
- **
- ** @sa ::VL_XSTRINGIFY
- **/
-
-#define VL_STRINGIFY(x) # x
-
-/** @brief Expand and then convert the argument to a string
- **
- ** @param x value to be macro-expanded and converted.
- **
- ** This macro macro-expands the argument @a x and stringifies the
- ** result of the expansion. For instance
- **
- ** @code
- ** #define A B
- ** char const * str = VL_STRINGIFY(A) ;
- ** @endcode
- **
- ** initializes <code>str</code> with a pointer to the string
- ** <code>"B"</code>.
- **
- ** @sa ::VL_STRINGIFY
- **/
-#define VL_XSTRINGIFY(x) VL_STRINGIFY(x)
-
-/** @brief Concatenate two arguments into a lexical unit
- **
- ** @param x first argument to be concatenated.
- ** @param y second argument to be concatenated.
- **
- ** This macro concatenates its arguments into a single lexical unit
- ** by means of the <code>##</code> preprocessor operator. Notice that
- ** arguments concatenated by <code>##</code> are not pre-expanded by
- ** the C preprocessor. To macro-expand the arguments and then
- ** concatenate them,use ::VL_XCAT.
- **
- ** @see ::VL_XCAT
- **/
-#define VL_CAT(x,y) x ## y
-
-/** @brief Expand and then concatenate two arguments into a lexical unit
- **
- ** @param x first argument to be concatenated.
- ** @param y second argument to be concatenated.
- **
- ** This macro is the same as ::VL_CAT, except that the arguments are
- ** macro expanded before being concatenated.
- **
- ** @see ::VL_CAT
- **/
-#define VL_XCAT(x,y) VL_CAT(x,y)
-
-/** @brief Expand and then concatenate three arguments into a lexical unit
- **
- ** @param x first argument to be concatenated.
- ** @param y second argument to be concatenated.
- ** @param z third argument to be concatenated.
- **
- ** This macro is the same as ::VL_XCAT, except that the arguments are three.
- **
- ** @see ::VL_XCAT
- **/
-#define VL_XCAT3(x,y,z) VL_XCAT(VL_XCAT(x,y),z)
-
-/** @} */
-
-/** @brief Convert a boolean to "yes" or "no" strings
- ** @param x boolean to convert.
- ** A pointer to either the string "yes" (if @a x is true)
- ** or the string "no".
- ** @par Example
- ** @code
- ** VL_PRINTF("Is x true? %s.", VL_YESNO(x))
- ** @endcode
- **/
-#define VL_YESNO(x) ((x)?"yes":"no")
 
 /* VL_HOST_H */
 #endif
