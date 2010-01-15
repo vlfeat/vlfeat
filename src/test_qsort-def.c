50e48a9d4b64f836a7418ff2baa2545e908cbc3d
Andrea Vedaldi <vedaldi@gmail.com>
2010-01-15 09:53:30 +0000

    new qsort-def.h: metaprogram implemeting quick sort

---
+++ src/test_qsort-def.c
@@ -0,0 +1,35 @@
+/** @file   test_heap-def.c
+ ** @brief  Test heap-def.h
+ ** @author Andrea Vedaldi
+ **/
+
+/* AUTORIGHTS
+ Copyright (C) 2007-09 Andrea Vedaldi and Brian Fulkerson
+
+ This file is part of VLFeat, available in the terms of the GNU
+ General Public License version 2.
+ */
+
+#define VL_QSORT_prefix my_qsort
+#define VL_QSORT_type   float
+#include <vl/qsort-def.h>
+
+#include <stdio.h>
+
+int
+main (int argc, char ** argv)
+{
+  float values [] = {.4, 12.3, 44.0, 1.2, 3.4, 5.6} ;
+  vl_size n = sizeof(values) / sizeof(values[0]) ;
+  vl_uindex i ;
+
+  for (i = 0 ; i < n ; ++i) printf("%5.2f ", values [i]) ;
+  printf("\n") ;
+
+  my_qsort_sort (values, n) ;
+
+  for (i = 0 ; i < n ; ++i) printf("%5.2f ", values [i]) ;
+  printf("\n") ;
+
+  return 0 ;
+}
