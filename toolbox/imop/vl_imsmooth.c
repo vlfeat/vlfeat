ff72b6ad6182b61ba355aedf18017c41df949233
Andrea Vedaldi <vedaldi@gmail.com>
2010-01-22 14:19:27 +0000

    vl_imsmooth.*: rewrite to use new styles and to simplify the code

--- toolbox/imop/vl_imsmooth.c
+++ toolbox/imop/vl_imsmooth.c
@@ -11,6 +11,125 @@ This file is part of VLFeat, available under the terms of the
 GNU GPLv2, or (at your option) any later version.
 */
 
+#ifdef VL_IMSMOOTH_INSTANTIATING
+
+#include <vl/float.th>
+#include <vl/host.h>
+
+#if (FLT == VL_TYPE_FLOAT)
+#define IMCONVCOL    vl_imconvcol_vf
+#define IMCONVCOLTRI vl_imconvcoltri_vf
+#else
+#define IMCONVCOL    vl_imconvcol_vd
+#define IMCONVCOLTRI vl_imconvcoltri_vd
+#endif
+
+VL_INLINE
+void
+VL_XCAT(_vl_imsmooth_smooth_, SFX)
+(T * outputImage,
+ vl_size numOutputRows,
+ vl_size numOutputColumns,
+ T const * inputImage,
+ vl_size numRows,
+ vl_size numColumns,
+ vl_size numChannels,
+ int kernel,
+ double sigma,
+ int step,
+ int flags)
+{
+  T * tempImage = (T*) mxMalloc (sizeof(T) * numRows * numOutputColumns) ;
+  vl_uindex k ;
+  vl_index j ;
+
+  /* Note that MATLAB uses a column major ordering, while VLFeat a row
+     major (standard) ordering for the image data. Effectively, VLFeat
+     is operating on a transposed image, but this is fine since filters
+     are symmetric.
+
+     Therefore:
+
+     input image width  = numRows
+     input image height = numColumns
+     output image width = numOutputRows (downsamped rows)
+     outout image height = numOutputColumns (downsampled columns)
+
+     In addition a temporary buffer is used. This is an image that
+     is obtained from the input image by convolving and downsampling
+     along the height and saving the result transposed:
+
+     temp image width  = numOutputColumns
+     temp image height = numRows
+  */
+
+  switch (kernel) {
+    case GAUSSIAN :
+    {
+      vl_size W = ceil (4.0 * sigma) ;
+      T * filter = (T*) mxMalloc (sizeof(T) * (2 * W + 1)) ;
+      T acc = 0 ;
+      for (j = 0 ; j < 2 * W + 1 ; ++j) {
+        T z = ( (T) j - W) / (sigma + VL_EPSILON_F) ;
+        filter[j] = exp(- 0.5 * (z*z)) ;
+        acc += filter[j] ;
+      }
+      for (j = 0 ; j < 2 * W + 1 ; ++j) {
+        filter[j] /= acc ;
+      }
+
+      for (k = 0 ; k < numChannels ; ++k) {
+
+        IMCONVCOL (tempImage, numOutputColumns,
+                   inputImage, numRows, numColumns, numRows,
+                   filter, -W, W, step, flags) ;
+
+        IMCONVCOL (outputImage, numOutputRows,
+                   tempImage, numOutputColumns, numRows, numOutputColumns,
+                   filter, -W, W, step, flags) ;
+
+        inputImage += numRows * numColumns ;
+        outputImage += numOutputRows * numOutputColumns ;
+      }
+      mxFree (filter) ;
+      break ;
+    }
+
+    case TRIANGULAR:
+    {
+      unsigned int W = VL_MAX((unsigned int) sigma, 1) ;
+      for (k = 0 ; k < numChannels ; ++k) {
+
+       IMCONVCOLTRI (tempImage, numOutputColumns,
+                     inputImage, numRows, numColumns, numRows,
+                     W, step, flags) ;
+
+       IMCONVCOLTRI (outputImage, numOutputRows,
+                     tempImage, numOutputColumns, numRows, numOutputColumns,
+                     W, step, flags) ;
+
+        inputImage += numRows * numColumns ;
+        outputImage += numOutputRows * numOutputColumns ;
+      }
+      break ;
+    }
+
+    default:
+      assert (0) ;
+      break ;
+  }
+  mxFree (tempImage) ;
+}
+
+#undef FLT
+#undef IMCONVCOLTRI
+#undef IMCONVCOL
+#undef VL_IMSMOOTH_INSTANTIATING
+
+/* ---------------------------------------------------------------- */
+/* VL_IMSMOOTH_INSTANTIATING */
+#else
+
 #include <mexutils.h>
 
 #include <vl/generic.h>
@@ -26,7 +145,7 @@ enum {
   opt_padding = 0,
   opt_subsample,
   opt_kernel,
-  opt_verbose 
+  opt_verbose
 } ;
 
 /* options */
@@ -40,12 +159,21 @@ uMexOption options [] = {
 
 enum {GAUSSIAN, TRIANGULAR} ;
 
+
+#define VL_IMSMOOTH_INSTANTIATING
+#define FLT VL_TYPE_FLOAT
+#include "vl_imsmooth.c"
+
+#define VL_IMSMOOTH_INSTANTIATING
+#define FLT VL_TYPE_DOUBLE
+#include "vl_imsmooth.c"
+
 void
-mexFunction(int nout, mxArray *out[], 
+mexFunction(int nout, mxArray *out[],
             int nin, const mxArray *in[])
 {
-  enum {IN_I=0, IN_S, IN_END} ;
-  enum {OUT_J=0} ;
+  enum {IN_I = 0, IN_S, IN_END} ;
+  enum {OUT_J = 0} ;
   int opt ;
   int next = IN_END ;
   mxArray const  *optarg ;
@@ -53,26 +181,25 @@ mexFunction(int nout, mxArray *out[],
   int padding = VL_PAD_BY_CONTINUITY ;
   int kernel = GAUSSIAN ;
   int flags ;
-  int subsample = 1 ;
-  int verb = 0 ;  
+  vl_size step = 1 ;
+  int verb = 0 ;
   double sigma ;
-  mxClassID classid ;  
-  
-  int M,N,K,j,k,ndims ;
-  int M_, N_ ;
-  mwSize dims_[3] ;
-  mwSize const *dims ;
-  
+  mxClassID classid ;
+
+  mwSize M, N, K, M_, N_, ndims ;
+  mwSize dims_ [3] ;
+  mwSize const * dims ;
+
   /* -----------------------------------------------------------------
    *                                               Check the arguments
-   * -------------------------------------------------------------- */ 
-  
+   * -------------------------------------------------------------- */
+
   if (nin < 2) {
     mexErrMsgTxt("At least two input arguments required.");
   } else if (nout > 1) {
     mexErrMsgTxt("Too many output arguments.");
   }
-    
+
   while ((opt = uNextOption(in, nin, options, &next, &optarg)) >= 0) {
     switch (opt) {
       case opt_padding :
@@ -80,7 +207,8 @@ mexFunction(int nout, mxArray *out[],
         enum {buflen = 32} ;
         char buf [buflen] ;
         if (!uIsString(optarg, -1)) {
-          mexErrMsgTxt("PADDING argument must be a string") ;
+          mxuError(vlmxErrInvalidArgument,
+                   "PADDING argument must be a string") ;
         }
         mxGetString(optarg, buf, buflen) ;
         buf [buflen - 1] = 0 ;
@@ -89,18 +217,22 @@ mexFunction(int nout, mxArray *out[],
         } else if (uStrICmp("continuity", buf) == 0) {
           padding = VL_PAD_BY_CONTINUITY ;
         } else {
-          mexErrMsgTxt("PADDING can be either ZERO or CONTINUITY") ;
+          mxuError(vlmxErrInvalidArgument,
+                   "PADDING must be either ZERO or CONTINUITY, was '%s'",
+                   buf) ;
         }
         break ;
       }
-        
+
       case opt_subsample :
-        if (!uIsRealScalar(optarg)) {
-          mexErrMsgTxt("SUBSAMPLE must be a scalar") ;
+        if (!vlmxIsPlainScalar(optarg)) {
+          mxuError(vlmxErrInvalidArgument,
+                   "SUBSAMPLE must be a scalar") ;
         }
-        subsample = *mxGetPr(optarg) ;
-        if (subsample < 1) {
-          mexErrMsgTxt("SUBSAMPLE must be not less than one") ;
+        step = *mxGetPr(optarg) ;
+        if (step < 1) {
+          mxuError(vlmxErrInvalidArgument,
+                   "SUBSAMPLE must be not less than one") ;
         }
         break ;
 
@@ -109,7 +241,8 @@ mexFunction(int nout, mxArray *out[],
         enum {buflen = 32} ;
         char buf [buflen] ;
         if (!uIsString(optarg, -1)) {
-          mexErrMsgTxt("KERNEL argument must be a string") ;
+          mxuError(vlmxErrInvalidArgument,
+                   "KERNEL argument must be a string") ;
         }
         mxGetString(optarg, buf, buflen) ;
         buf [buflen - 1] = 0 ;
@@ -118,7 +251,9 @@ mexFunction(int nout, mxArray *out[],
         } else if (uStrICmp("triangular", buf) == 0) {
           kernel = TRIANGULAR ;
         } else {
-          mexErrMsgTxt("Unknown kernel type") ;
+          mxuError(vlmxErrInvalidArgument,
+                   "Unknown kernel type '%s'",
+                   buf) ;
         }
         break ;
       }
@@ -126,46 +261,48 @@ mexFunction(int nout, mxArray *out[],
       case opt_verbose :
         ++ verb ;
         break ;
-                
-      default: 
+
+      default:
         assert(0) ;
     }
   }
-    
-  if (!uIsRealScalar(in[IN_S])) {
-    mexErrMsgTxt("S must be a real scalar") ;
+
+  if (! vlmxIsPlainScalar(IN(S))) {
+    mxuError(vlmxErrInvalidArgument,
+             "S must be a real scalar") ;
   }
-  
-  classid = mxGetClassID(in[IN_I]) ;
-  
+
+  classid = mxGetClassID(IN(I)) ;
+
   if (classid != mxDOUBLE_CLASS &&
       classid != mxSINGLE_CLASS) {
-    mexErrMsgTxt("I must be either DOUBLE or SINGLE.") ;
+    mxuError(vlmxErrInvalidArgument,
+             "I must be either DOUBLE or SINGLE") ;
   }
-  
-  if (mxGetNumberOfDimensions(in[IN_I]) > 3) {
-    mexErrMsgTxt("I must be either a two or three dimensional array.") ;
+  if (mxGetNumberOfDimensions(IN(I)) > 3) {
+    mxuError(vlmxErrInvalidArgument,
+             "I must be either a two or three dimensional array") ;
   }
-  
-  ndims = mxGetNumberOfDimensions(in[IN_I]) ;
-  dims = mxGetDimensions(in[IN_I]) ;
+
+  ndims = mxGetNumberOfDimensions(IN(I)) ;
+  dims = mxGetDimensions(IN(I)) ;
   M = dims[0] ;
   N = dims[1] ;
   K = (ndims > 2) ? dims[2] : 1 ;
 
-  sigma = *mxGetPr(in[IN_S]) ;
-  if ((sigma < 0.01) && (subsample == 1)) {    
-    out[OUT_J] = mxDuplicateArray(in[IN_I]) ;
+  sigma = * mxGetPr(IN(S)) ;
+  if ((sigma < 0.01) && (step == 1)) {
+    OUT(J) = mxDuplicateArray(IN(I)) ;
     return ;
   }
-  
-  M_ = (M - 1) / subsample + 1 ;
-  N_ = (N - 1) / subsample + 1 ;
+
+  M_ = (M - 1) / step + 1 ;
+  N_ = (N - 1) / step + 1 ;
   dims_ [0] = M_ ;
   dims_ [1] = N_ ;
   if (ndims > 2) dims_ [2] = ndims ;
-  
-  out[OUT_J] = mxCreateNumericArray(ndims, dims_, classid, mxREAL) ;
+
+  OUT(J) = mxCreateNumericArray(ndims, dims_, classid, mxREAL) ;
 
   if (verb) {
     char const *classid_str = 0, *kernel_str = 0, *padding_str = 0 ;
@@ -184,40 +321,44 @@ mexFunction(int nout, mxArray *out[],
       case TRIANGULAR: kernel_str = "triangular" ; break ;
       default: assert (0) ; break ;
     }
-    
-    mexPrintf("imsmooth: [%dx%d] -> [%dx%d] (%s, sampling per. %d)\n", 
-              N, M, N_, M_, classid_str, subsample) ;
-    mexPrintf("          padding: %s\n", padding_str) ;
-    mexPrintf("          kernel: %s\n", kernel_str) ;
-    mexPrintf("          sigma: %g\n", sigma) ;
-    mexPrintf("          SIMD enabled: %s\n", vl_get_simd_enabled() ? "yes" : "no") ;
+
+    mexPrintf("vl_imsmooth: [%dx%d] -> [%dx%d] (%s, subsampling step %d)\n",
+              N, M, N_, M_, classid_str, step) ;
+    mexPrintf("vl_imsmooth: padding: %s\n", padding_str) ;
+    mexPrintf("vl_imsmooth: kernel: %s\n", kernel_str) ;
+    mexPrintf("vl_imsmooth: sigma: %g\n", sigma) ;
+    mexPrintf("vl_imsmooth: SIMD enabled: %s\n",
+              vl_get_simd_enabled() ? "yes" : "no") ;
   }
-  
+
   /* -----------------------------------------------------------------
    *                                                        Do the job
-   * -------------------------------------------------------------- */ 
+   * -------------------------------------------------------------- */
   flags  = padding ;
   flags |= VL_TRANSPOSE ;
-  
+
   switch (classid) {
-    case mxSINGLE_CLASS: 
-#undef FLT
-#undef VL_IMCONVCOL
-#undef VL_IMCONVCOLTRI
-#define FLT float
-#define VL_IMCONVCOL vl_imconvcol_vf
-#define VL_IMCONVCOLTRI vl_imconvcoltri_vf
-#include "imsmooth.tc"
+    case mxSINGLE_CLASS:
+      _vl_imsmooth_smooth_f ((float*) mxGetPr(OUT(J)),
+                             M_, N_,
+                             (float const*) mxGetPr(IN(I)),
+                             M, N, K,
+                             kernel, sigma, step, flags) ;
       break ;
+
     case mxDOUBLE_CLASS:
-#undef FLT
-#undef VL_IMCONVCOL
-#undef VL_IMCONVCOLTRI
-#define FLT double 
-#define VL_IMCONVCOL vl_imconvcol_vd
-#define VL_IMCONVCOLTRI vl_imconvcoltri_vd
-#include "imsmooth.tc"
+      _vl_imsmooth_smooth_d ((double*) mxGetPr(OUT(J)),
+                             M_, N_,
+                             (double const*) mxGetPr(IN(I)),
+                             M, N, K,
+                             kernel, sigma, step, flags) ;
       break ;
-    default: assert (0) ; break ;
+
+    default:
+      assert(0) ;
+      break ;
   }
 }
+
+/* VL_IMSMOOTH_INSTANTIATING */
+#endif
--- toolbox/imop/vl_imsmooth.m
+++ toolbox/imop/vl_imsmooth.m
@@ -1,9 +1,26 @@
 % VL_IMSMOOTH  Smooth image
-%   I=VL_IMSMOOTH(I,SIGMA) convolves the image I by an isotropic Gaussian
-%   kernel of standard deviation SIGMA.  I must be an array of
-%   doubles. IF the array is three dimensional, the third dimension is
-%   assumed to span different channels (e.g. R,G,B). In this case,
+%   J = VL_IMSMOOTH(I,SIGMA) convolves the image I by an isotropic
+%   Gaussian kernel of standard deviation SIGMA.  I must be an array
+%   of doubles. IF the array is three dimensional, the third dimension
+%   is assumed to span different channels (e.g. R,G,B). In this case,
 %   each channel is convolved independently.
+%
+%   VL_IMSMOOTH() accepts the following options:
+%
+%   Kernel::
+%     Selects between GAUSSIAN and TRINAGULAR kernels. The triangular
+%     kernel support has 2*SIGMA-1 sampels. Kernels have unit mass.
+%
+%   Padding::
+%     Selects between ZERO or CONTINUITY padding method to handle the
+%     image boundaries. ZERO extends the input image with zeroes
+%     around the border, and CONTINUITY extends the image with
+%     constant pixels.
+%
+%   Step::
+%     Sets the subsampling step. A subsampling step of STEP pixels
+%     causes J(1:STEPS:end, 1:STEPS:end, :) to be computed. This is
+%     useful to downsample the image.
 %
 %   See also:: VL_HELP().
 
