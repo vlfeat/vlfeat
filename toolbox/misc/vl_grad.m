8f20dffd47e63ea3235ea2efc506912af0111599
Andrea Vedaldi <vedaldi@gmail.com>
2010-06-24 22:28:19 +0100

    vl_grad.m: comments

--- toolbox/misc/vl_grad.m
+++ toolbox/misc/vl_grad.m
@@ -1,13 +1,15 @@
 function [Ix,Iy] = vl_grad(I,varargin) ;
 % VL_GRAD Compute the gradient of an image
-%   [Ix,Iy] = VL_GRAD(I) returns the finite differences gradient of
-%   the image I. I must be a 2D array. The function uses central
-%   differences and for all but the boundaries pixels, for which it
-%   uses forward/backward differences as appropriate.
+%   [IX,IY] = VL_GRAD(I) returns the finite gradient components IX,IY
+%   of the grayscale image I. I must be a two-dimensional matrix. The
+%   function uses central differences for all but the boundary pixels,
+%   for which it uses forward/backward differences as appropriate.
 %
-%   VL_GRAD(I,'forward') uses only forward differences (except on the
-%   lower and right boundaries, where backward difference is used).
-%   Similarly, VL_GRAD(I,'backward') uses only backward differences.
+%   The function accepts the following options:
+%
+%   Type:: Central
+%     Use either 'central', 'forward', or 'backward' differences for
+%     all but the boundary pixels.
 %
 %   See also: GRADIENT(), VL_HELP().
 
@@ -37,4 +39,3 @@ switch opts.type
   otherwise
     error('Difference type ''%s'' unknown.', opts.type) ;
 end
-
