7c7b9fc0eec4dd2198c9df7e86cb12302fe67207
Andrea Vedaldi <vedaldi@gmail.com>
2011-01-28 11:58:55 +0000

    vl_grad.m: improves documentation and adds test unit

--- toolbox/misc/vl_grad.m
+++ toolbox/misc/vl_grad.m
@@ -1,15 +1,22 @@
 function [Ix,Iy] = vl_grad(I,varargin) ;
 % VL_GRAD Compute the gradient of an image
-%   [IX,IY] = VL_GRAD(I) returns the finite gradient components IX,IY
-%   of the grayscale image I. I must be a two-dimensional matrix. The
-%   function uses central differences for all but the boundary pixels,
-%   for which it uses forward/backward differences as appropriate.
+%   [IX,IY] = VL_GRAD(I) returns the gradient components IX,IY of the
+%   2-D discrete function I. I must be a two-dimensional
+%   matrix. VL_GRAD() computes the gradient by using finite
+%   differences; specifically, it uses central differences for all but
+%   the boundary pixels, for which it uses forward/backward
+%   differences as appropriate.
+%
+%   Remark::
+%     VL_GRAD() is similar to the MATLAB built-in GRADIENT() function,
+%     excepts that it supports different gradient approximations.
 %
 %   VL_GRAD() accepts the following options:
 %
-%   Type:: Central
-%     Use either 'central', 'forward', or 'backward' differences for
-%     all but the boundary pixels.
+%   Type:: central
+%     Specify which type of finite differences to use for all but the
+%     boundary samples. TYPE can be one of 'central', 'forward', or
+%     'backward'.
 %
 %   See also: GRADIENT(), VL_HELP().
 
---
+++ toolbox/xtest/vl_test_grad.m
@@ -0,0 +1,22 @@
+function results = vl_test_grad(varargin)
+% VL_TEST_GRAD
+vl_test_init ;
+
+function s = setup()
+s.I = rand(150,253) ;
+s.I_small = rand(2,2) ;
+
+function test_equiv(s)
+vl_assert_equal(gradient(s.I), vl_grad(s.I)) ;
+
+function test_equiv_small(s)
+vl_assert_equal(gradient(s.I_small), vl_grad(s.I_small)) ;
+
+function test_equiv_forward(s)
+Ix = diff(s.I,2,1) ;
+Iy = diff(s.I,2,1) ;
+
+vl_assert_equal(gradient(s.I_small), vl_grad(s.I_small)) ;
+
+
+ 
