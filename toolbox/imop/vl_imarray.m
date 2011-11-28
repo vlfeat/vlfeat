0226ddf07147a4377981303a24e488f5ec663293
Andrea Vedaldi <vedaldi@gmail.com>
2011-11-28 10:39:38 +0000

    vl_imarray*.m: improves handling of colour image arrays

--- toolbox/imop/vl_imarray.m
+++ toolbox/imop/vl_imarray.m
@@ -1,8 +1,8 @@
 function J = vl_imarray(A,varargin)
 % VL_IMARRAY  Flattens image array
-%   J=VL_IMARRAY(A) creates a composite image J from the array of
-%   images A. A can be either a M*N*K array, storing one gray-scale
-%   image per slice, or a M*N*3*K or M*N*K*3 array, storing one RGB
+%   J=VL_IMARRAY(A) creates a mosaic J from the array of images A. A
+%   can be either a M*N*K array, storing one gray-scale image per
+%   slice, or a M*N*3*K or M*N*K*3 array, storing a true color RGB
 %   image per slice. The function returns an image J which is a tiling
 %   of the images in the array. Tiles are filled from left to right
 %   and top to bottom.
@@ -11,24 +11,25 @@ function J = vl_imarray(A,varargin)
 %
 %   VL_IMARRAY() accepts the following options:
 %
-%   Spacing:: [0]
+%   Spacing:: 0
 %     Separate the images by a border of the specified width (the
 %     border is assigned 0 value, which usually corresponds to black).
 %
-%   Layout:: [empty]
+%   Layout:: empty
 %     Specify a vector [TM TN] with the number of rows and columns of
 %     the tiling. If equal to [] the layout is computed automatically.
 %
-%   Movie:: [false]
-%     Display/return a movie rather than generating a tiling.
+%   Movie:: false
+%     Display or return a movie instead of generating a tiling.
 %
-%   CMap:: [empty]
+%   CMap:: []
 %     Specify a colormap to construct a movie when the input is an
 %     indexed image array. If not specified, MATLAB default colormap
 %     is used.
 %
-%   Reverse:: [true]
-%     Start filling the grid from the bottom rather than from the top.
+%   Reverse:: true
+%     Start filling the mosaic tiles from the bottom rather than from
+%     the top.
 %
 %   See also:VL_IMARRAYSC(), VL_HELP().
 
@@ -58,7 +59,7 @@ else
     swap3 = true ;
     [height,width,numImages,numChannels] = size(A) ;
   else
-    error('A must be either M*N*K or M*N*3*K or M*N*K*3.') ;
+    error('A is neither M*N*K, nor M*N*3*K, nor M*N*K*3.') ;
   end
 end
 
