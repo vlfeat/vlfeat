5da12d30414937043afc98b893ca70ea73ac467a
Andrea Vedaldi <vedaldi@gmail.com>
2012-01-30 19:10:35 +0000

    apps/phow_caltech101.m: fixes call to vl_homkermap

--- apps/phow_caltech101.m
+++ apps/phow_caltech101.m
@@ -302,7 +302,7 @@ function [className, score] = classify(model, im)
 % -------------------------------------------------------------------------
 
 hist = getImageDescriptor(model, im) ;
-psix = vl_homkermap(hist, 1, .7, 'kchi2') ;
+psix = vl_homkermap(hist, 1, 'kchi2', 'period', .7) ;
 scores = model.w' * psix + model.b' ;
 [score, best] = max(scores) ;
 className = model.classes{best} ;
