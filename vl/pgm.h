5b8ffa75a3c0f0c211cae0901bba015f3335c734
Andrea Vedaldi <vedaldi@gmail.com>
2012-06-03 21:47:57 +0100

    pgm.h: improves style

--- vl/pgm.h
+++ vl/pgm.h
@@ -20,12 +20,10 @@ the terms of the BSD license (see the COPYING file).
 
 /** @name PGM parser error codes
  ** @{ */
-
 #define VL_ERR_PGM_INV_HEAD  101 /**< Invalid PGM header section */
 #define VL_ERR_PGM_INV_META  102 /**< Invalid PGM meta section */
 #define VL_ERR_PGM_INV_DATA  103 /**< Invalid PGM data section */
 #define VL_ERR_PGM_IO        104 /**< Generic I/O error */
-
 /** @} */
 
 /** @brief PGM image meta data
@@ -37,37 +35,37 @@ the terms of the BSD license (see the COPYING file).
 
 typedef struct _VlPgmImage
 {
-  int        width ;     /**< image width.                     */
-  int        height ;    /**< image height.                    */
-  int        max_value ; /**< pixel maximum value (<= 2^16-1). */
-  vl_bool    is_raw ;    /**< is RAW format?                   */
+  vl_size width ;      /**< image width.                     */
+  vl_size height ;     /**< image height.                    */
+  vl_size max_value ;  /**< pixel maximum value (<= 2^16-1). */
+  vl_bool is_raw ;     /**< is RAW format?                   */
 } VlPgmImage ;
 
 /** @name Core operations
  ** @{ */
-VL_EXPORT int vl_pgm_extract_head  (FILE *f, VlPgmImage       *im) ;
-VL_EXPORT int vl_pgm_extract_data  (FILE *f, VlPgmImage const *im, void *data) ;
-VL_EXPORT int vl_pgm_insert        (FILE *f,
-                                    VlPgmImage const *im,
-                                    void const*data ) ;
-VL_EXPORT int vl_pgm_get_npixels   (VlPgmImage const *im) ;
-VL_EXPORT int vl_pgm_get_bpp       (VlPgmImage const *im) ;
+VL_EXPORT int vl_pgm_extract_head (FILE *f, VlPgmImage *im) ;
+VL_EXPORT int vl_pgm_extract_data (FILE *f, VlPgmImage const *im, void *data) ;
+VL_EXPORT int vl_pgm_insert (FILE *f,
+                             VlPgmImage const *im,
+                             void const*data ) ;
+VL_EXPORT int vl_pgm_get_npixels (VlPgmImage const *im) ;
+VL_EXPORT int vl_pgm_get_bpp (VlPgmImage const *im) ;
 /** @} */
 
 /** @name Helper functions
  ** @{ */
-VL_EXPORT int vl_pgm_write         (char const *name,
-                                    vl_uint8 const *data,
-                                    int width, int height) ;
-VL_EXPORT int vl_pgm_write_f       (char const *name,
-                                    float const *data,
-                                    int width, int height) ;
-VL_EXPORT int vl_pgm_read_new      (char const *name,
-                                    VlPgmImage *im,
-                                    vl_uint8 **data) ;
-VL_EXPORT int vl_pgm_read_new_f    (char const *name,
-                                    VlPgmImage *im,
-                                    float **data) ;
+VL_EXPORT int vl_pgm_write (char const *name,
+                            vl_uint8 const *data,
+                            int width, int height) ;
+VL_EXPORT int vl_pgm_write_f (char const *name,
+                              float const *data,
+                              int width, int height) ;
+VL_EXPORT int vl_pgm_read_new (char const *name,
+                               VlPgmImage *im,
+                               vl_uint8 **data) ;
+VL_EXPORT int vl_pgm_read_new_f (char const *name,
+                                 VlPgmImage *im,
+                                 float **data) ;
 
 /** @} */
 
