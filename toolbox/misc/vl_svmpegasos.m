ffc51ff1f288071663af8627d37233907ca59d9f
Daniele Perrone <dnlperr@gmail.com>
2012-07-23 16:35:15 +0200

    Matlab Documentation

--- toolbox/misc/vl_svmpegasos.m
+++ toolbox/misc/vl_svmpegasos.m
@@ -1,19 +1,21 @@
-% VL_PEGASOS PEGASOS linear SVM solver
-%   W = VL_PEGASOS(X, Y, LAMBDA) learns a linear SVM W given training
-%   vectors X, their labels Y, and the regularization parameter LAMBDA
-%   using the PEGASOS [1] solver. The algorithm finds a minimizer W of
-%   the objective function
+% VL_SVMPEGASOS PEGASOS linear SVM solver
+%   W = VL_SVMPEGASOS(DATA, LAMBDA) learns a linear SVM W given training
+%   struct DATA, and the regularization parameter LAMBDA using the
+%   PEGASOS [1] solver. The algorithm finds a minimizer W of the
+%   objective function
 %
 %     LAMBDA/2 |W|^2 + 1/N SUM_i LOSS(W, X(:,i), Y(i))
 %
 %   where LOSS(W,X,Y) = MAX(0, 1 - Y W'X) is the hinge loss and N is
 %   the number of training vectors in X.
 %
-%   [W B INFO] = VL_SVMPEGASOS(X, Y, LAMBDA) learns a linear SVM W
-%   and a bias B given training vectors X, their labels Y, and the
-%   regularization parameter LAMBDA using the PEGASOS [1]
-%   solver. INFO is a struct containing the input parameters plus
-%   diagnostic informations:
+%   The training struct DATA is created using the function
+%   VL_MAKETRAININGDATA.
+% 
+%   [W B INFO] = VL_SVMPEGASOS(DATA, LAMBDA) learns a linear SVM W
+%   and a bias B given training struct DATA, and the regularization
+%   parameter LAMBDA using the PEGASOS [1] solver. INFO is a struct
+%   containing the input parameters plus diagnostic informations:
 % 
 %   energy::
 %     SVM energy value.
@@ -87,7 +89,7 @@
 %     the error term. A common application is to balance an unbalanced
 %     dataset.
 %
-%   DiagnosticFunction:: [empty]
+%   DiagnosticFunction:: [empty] 
 %     Specify a function handle to be called every ENERGYFREQ iterations.
 % 
 %   DiagnosticCallRef:: [empty]
@@ -96,42 +98,12 @@
 %   EnergyFreq:: [100]
 %     Specify how often the SVM energy is computed.
 % 
-%   HOMKERMAP:: [empty]
-%     Specify the use of an Homogeneus Kernel map for the training
-%     data (See [2],[3]). The passed value N is such that a 2*N+1
-%     dimensional approximated kernel map is computed. Each
-%     training data point is expanded online into a vector of
-%     dimension 2*N+1.
-%  
-%   KChi2::
-%     Compute the map for the Chi2 kernel.
+%   ValidationData::
+%     Specify a validation dataset. The validation dataset must be
+%     created using VL_MAKETRAININGSET. If specified, the energy
+%     value and all the diagnostic informations are computed on the
+%     validation dataset, otherwise the training dataset is used.
 %
-%   KINTERS::
-%     Compute the map for the intersection kernel.
-%
-%   KL1::
-%     Same as KINTERS, but deprecated as the name is not fully
-%     accurate.
-%
-%   KJS::
-%     Compute the map for the JS (Jensen-Shannon) kernel.
-%
-%   Period:: [automatically tuned]
-%     Set the period of the kernel specturm. The approximation is
-%     based on periodicizing the kernel specturm. If not specified,
-%     the period is automatically set based on the heuristic described
-%     in [2].
-%
-%   Window:: [RECTANGULAR]
-%     Set the window used to truncate the spectrum before The window
-%     can be either RECTANGULAR or UNIFORM window. See [2] and the API
-%     documentation for details.
-%
-%   Gamma:: [1]
-%     Set the homogeneity degree of the kernel. The standard kernels
-%     are 1-homogeneous, but sometimes smaller values perform better
-%     in applications. See [2] for details.
-% 
 %   Verbose::
 %     Be verbose.
 %
@@ -140,13 +112,15 @@
 %     to continue training. I.e., the command
 %
 %       vl_twister('state',0) ;
-%       w = vl_pegasos(x,y,lambda,'NumIterations',1000) ;
+%       dataset = vl_maketrainingset(x,y) ;
+%       w = vl_svmpegasos(dataset,lambda,'NumIterations',1000) ;
 %
 %     produces the same result as the sequence
 %
 %       vl_twister('state',0) ;
-%       w = vl_pegasos(x,y,lambda,'NumIterations',500) ;
-%       w = vl_pegasos(x,y,lambda,'NumIterations',500, ...
+%       dataset = vl_maketrainingset(x,y) ;
+%       w = vl_svmpegasos(dataset,lambda,'NumIterations',500) ;
+%       w = vl_svmpegasos(dataset,lambda,'NumIterations',500, ...
 %                      'StartingIteration', 501, ...
 %                      'StartingModel', w) ;
 %
@@ -154,15 +128,7 @@
 %     [1] S. Shalev-Shwartz, Y. Singer, N. Srebro, and
 %     A. Cotter. Pegasos: Primal Estimated sub-GrAdient SOlver for
 %     SVM. MBP, 2010.
-% 
-%     [2] A. Vedaldi and A. Zisserman
-%     `Efficient Additive Kernels via Explicit Feature Maps',
-%     Proc. CVPR, 2010.
 %
-%     [3] A. Vedaldi and A. Zisserman
-%     `Efficient Additive Kernels via Explicit Feature Maps',
-%     PAMI, 2011 (submitted).
-%
-%   See also: VL_HOMKERMAP(), VL_HELP().
+%   See also: VL_MAKETRAININGSET(), VL_HOMKERMAP(), VL_HELP().
 
 % AUTHORIGHTS
