bd22ba9a8dedc9b29e800bf75b670563f0f5cd12
Andrea Vedaldi <vedaldi@gmail.com>
2012-09-25 18:28:19 +0100

    vl_covdet.m: adds documentation

--- toolbox/sift/vl_covdet.m
+++ toolbox/sift/vl_covdet.m
@@ -1,139 +1,118 @@
 % VL_COVDET  Covariant feature detectors and descriptors
-%   VL_COVDET() detects covariant features such as SIFT [1], Hessian,
-%   Harris keypoints, their scale and adapted variants [2], and
-%   computes the corresponding SIFT descriptors.
+%   VL_COVDET() implements a number of co-variant feature detectors
+%   (e.g., DoG, Harris-Affine, Harris-Laplace) and allow to compute
+%   corresponding feature descriptors (SIFT, raw patches).
 %
-%   F = VL_COVDET(I) computes the SIFT features for image I [1]. F is
-%   a matirx storing the frame of one SIFT keypoint per column in the
-%   [X;Y;S;TH] format, where X, Y is the frame center, S its scale,
-%   and TH its orientation.
+%   F = VL_COVDET(I) detects upright scale and translation covariant
+%   features based on the Difference of Gaussian (Dog) cornerness
+%   measure from image I (a grayscale image of class SINGLE). F is in
+%   the format of oriented ellipse feature frames (see VL_PLOTFRAME()
+%   for the definition) even if features are only scale-invariant
+%   (discs or oriented discs).
 %
-%   VL_COVDET(I, 'Method', M) selects a feature extraction method
-%   from:
+%   VL_COVDET(I, 'Method', METHOD) allows using one of the
+%   following methods instead:
 %
-%   DoG:: Difference of Gaussian operator
-%     This is the default method and the method used by SIFT. Features
-%     are detected as local maxima (in space and scale) of the
-%     Lapalacian operator response.
+%   DoG::
+%     The Difference of Gaussians is an approximate version of the
+%     multiscale trace of Laplacian operator [].
 %
-%   Hessian:: Hessian operator
-%     Features are detected as local maxima (in space and scale) of
-%     the Hessian operator response.
+%   Hessian::
+%     Determinant Hessian operator [].
 %
-%   F = VL_COVDET(I, 'AffineAdaptation', true) turns on the affine
-%   adapation of the features based on the second moment
-%   matrix. VL_COVDET(I, 'Orientation', false) turns off the detection
-%   of the feature orientation (so that features are taken to be
-%   upright). Based on different combinations of these options, the
-%   format of the feature frames F changes as follows:
+%   HessianLaplace::
+%     Determinant of Hessian for space localisation, trace of
+%     Laplacian for scale detection [].
 %
-%     AffineAdaptation  Orientation Frame Type        Frame format
-%     false*            false       disc              [X;Y;S]
-%     false*            true*       oriented disc     [X;Y;S;TH]
-%     true              false       ellipse           [X;Y;E11;E12;E22]
-%     true              true*       oriented ellipse  [X;Y;A11;A12;A21;A22]
+%   HarrisLaplace::
+%     Harris cornerness measure for space localisation, trace
+%     of Laplacian for scale detection [].
 %
-%   where * denotes the default value and the parameters are given by:
+%   MultiscaleHessian::
+%     Same as HessianLaplace, but Laplacian scale detection is not
+%     performend (features are simply detected at multiple scales) [].
 %
-%   [X, Y]:: coordinate center
-%   S:: scale
-%   TH:: orientation (in radians)
-%   [E11,E12,E22]:: ellpitical shape
-%      This is in the format , which is the
-%      inverse...
-%   [A11,A12;A21,A22]::
-%      Matirx A mapping
+%   MultiscaleHarris::
+%     Same as HarrisLaplace, but Laplacian scale detection is not
+%     performend (features are simply detected at multiple scales) [].
+%
+%   The number of detected features is affected by the
+%   'PeakThreshold', which sets the minimum absolute vale of the
+%   cornerness measure to accept a feature. A larger threshold select
+%   fewer features. To adjust the threshold, the score of the detected
+%   features can be obtained in the INFO structure (see later).
+%
+%   Features can also be filtered by setting the 'EdgeThreshold'
+%   parameters, which sets an upper bound on the ratio of the maxium
+%   over the minium curvature of the cornerness measure at the
+%   detected location. The idea is that unbalanced curvatures
+%   correspond to features detected along image edges, and should
+%   therefore be discarded as spatially unstable.
+%
+%   VL_COVDET(..., 'EstimateAffineShape', true) switches on affine
+%   adaptation, which attempts to estimate the affine co-variant shape
+%   of each feature based on the algorihtm of [].
+%
+%   VL_COVDET(..., 'EstimateOrientations', true) switches on the
+%   estimation of the orientation of features (which are therefore not
+%   upright anymore) []. Note that more than one orientation can be
+%   associated to each feature, creating copies of them.
+%
+%   VL_COVDET(..., 'Frames', f) allows to specify user defined
+%   frames. This skips detection, but estimating the affine shape
+%   or the orietnations can still be applied. Moreover, descriptors
+%   for these frames can be computed.
 %
 %   [F,D] = VL_COVDET(I, ...) computes the SIFT descriptors [1] for
 %   the detected features. Each column of D is the descriptor of the
 %   corresponding frame in F. A descriptor is a 128-dimensional vector
-%   of class UINT8 or DOUBLE when 'FloatDescriptors' option is
-%   specified.
+%   of class SINGLE. The same format of VL_SIFT() is used. SIFT
+%   features are computed on normalised image patches that are
+%   affected by the parameters explained next (for example, to comptue
+%   SIFT on a larger measurement reagion, increase the value of
+%   PatchRelativeExtent.
 %
-%   [F,D,GSS,RESP] = VL_COVDET(I, ...) returns the Gaussian scale
-%   space GSS of the image I and the response scale space RESP of the
-%   operator used to detect features in space and scale (its nature
-%   depends on the 'Method' option). For the format of GSS and RESP
-%   see VL_PLOTSS().
+%   [F,D] = VL_COVDET(I, 'descriptor', 'patch') can be used to extract
+%   raw patches instead of SIFT descriptors. In this case, each column
+%   of D is a stacked square image patch. The following parameters
+%   can be used to control the produced patches:
 %
-%   [F,D] = VL_COVDET(I,...,'Frames',FI) takes the frames FI as input,
-%   converts them to frames of a different type F asspecified by the
-%   parameters 'AffineAdaptation' and 'Orientation' as described above
-%   before.
+%   PatchResolution:: 15
+%     The size of the patch R in pixel. Specifically, the patch is a
+%     square of side 2*R+1 pixels.
 %
-%   VL_COVDET(...,'OptionName',value) accepts the following options:
+%   PatchRelativeExtent:: 7.5
+%     The extent E of the patch in the feature frame. A feature F
+%     define a mapping from the feature reference frame to the image
+%     reference frame as an affine transformation A,T (see
+%     VL_PLOTFRAME()). The patch is a square [-E, E]^2 in this frame
+%     (transform this square by A,T to find the extent in the image).
 %
-%   Octaves:: [maximum possible]
-%     Set the number of octave of the DoG scale space.
+%   PatchRelativeSigma: 1.0
+%     The smoothing SIGMA of the patch in the patch frame. The
+%     computed patch can be thought as being obtained by first
+%     warping the image (as a continous signal) by A,T, then
+%     smoothing the results by SIGMA, and then sampling.
 %
-%   Levels:: [3]
-%     Set the number of levels per octave of the DoG scale space.
+%   [F,D,INFO] = VL_COVDET(...) returns an additiona structure INFO
+%   with the following members:
 %
-%   FirstOctave:: [0]
-%     Set the index of the first octave of the DoG scale space. If
-%     FirstOctave < 0 the image I is upsampled and for FirstOctave > 0
-%     image is downsampled.
+%   info.peakScores::
+%     The peak scores of the detected features.
 %
-%   PeakThresh:: [0 for DoG, 28.5 for Hessian]
-%     Set the peak selection threshold.
+%   info.edgeScores::
+%     The edge scores of the detected features.
 %
-%   EdgeThresh:: [10]
-%     Set the non-edge selection threshold.
+%   info.gss::
+%     The Gaussian scale space (see VL_PLOTSS()).
 %
-%   NormThresh:: [-inf]
-%     Set the minimum l2-norm of the descriptors before
-%     normalization. Descriptors below the threshold are set to zero.
+%   info.css::
+%     The cornerness measure scale space (see VL_PLOTSS()).
 %
-%   Magnif:: [3]
-%     Set the descriptor magnification factor. The scale of the
-%     keypoint is multiplied by this factor to obtain the
-%     neighbourhood used for descriptor calculation.  In the case of
-%     isotropic frames (discs) it defines the size of the spatial
-%     bins. For instance, if there are 4 spatial bins along each
-%     spatial direction, the ``side'' of the descriptor is
-%     approximatively 4 * MAGNIF. In the case of anisotropic frames
-%     (ellipses) it defines the size of keypoint neighbourhood which
-%     is normalised into a square patch used fort descriptor
-%     computation.
-%
-%   WindowSize:: [2]
-%     Set the variance of the Gaussian window that determines the
-%     descriptor support. It is expressend in units of spatial
-%     bins. Used only for isotropic frames (discs).
-%
-%   AffWinSize:: [19]
-%     Size of the window used for Second Moment Matrix (SMM)
-%     calculation. SMM is used as an estimation of the affine
-%     shape. Used only for anisotropic frames (ellipses).
-%
-%   AffMaxIter:: [16]
-%     Maximum number of iterations for the affine shape estimation.
-%     If the convergence criterion is not fulfilled in these steps,
-%     keypoint is rejected. Used only for anisotropic frames (ellipses).
-%
-%   AffConvThr:: [0.05]
-%     Convergence criterion for affine shape estimation.
-%     Affine shape is accepted when:
-%
-%           \lambda_min(SMM)
-%       1 - ----------------  < AffConvThr
-%           \lambda_max(SMM)
-%
-%     Used only for anisotropic frames (ellipses).
-%
-%   SIFTPatchSize:: [41]
-%     Size of the patch used for SIFT descriptor calculation. Used only
-%     for anisotropic frames (ellipses).
-%
-%   Frames:: [not specified]
-%     If specified, set the frames to use (bypass the detector).
-%
-%   FloatDescriptors::
-%     If specified, descriptors are returned as float numbers.
+%   The function supports the following options:
 %
 %   Verbose::
-%     If specfified, be verbose (may be repeated to increase the
-%     verbosity level).
+%     If specified, it incerases the verbosity level.
 %
 %   REFERENCES::
 %     [1] D. G. Lowe, Distinctive image features from scale-invariant
@@ -142,9 +121,9 @@
 %     [2] K. Mikolajcyk and C. Schmid, An affine invariant interest
 %     point detector. ICCV, vol. 2350, pp. 128-142, 2002.
 %
-%   See also: VL_UBCMATCH(), VL_DSIFT(), VL_HELP().
+%   See also: VL_SIFT(), VL_PLOTFRAME(), VL_PLOTSS(), VL_HELP().
 
-% Copyright (C) 2007-12 Andrea Vedaldi and Brian Fulkerson.
+% Copyright (C) 2007-12 Karel Lenc, Andrea Vedaldi, and Michal Perdoch
 % All rights reserved.
 %
 % This file is part of the VLFeat library and is made available under
