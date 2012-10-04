09455e19c38bf6dfa611d90fa588fe9f9769a09b
Andrea Vedaldi <vedaldi@gmail.com>
2012-10-04 10:14:37 +0100

    sift: fixes the parsing of one of the options

--- src/sift.c
+++ src/sift.c
@@ -73,7 +73,7 @@ struct option const longopts [] = {
   { "help",            no_argument,            0,          'h'              },
   { "octaves",         required_argument,      0,          'O'              },
   { "levels",          required_argument,      0,          'S'              },
-  { "output",          optional_argument,      0,          'o'              },
+  { "output",          required_argument,      0,          'o'              },
   { "meta",            optional_argument,      0,          opt_meta         },
   { "frames",          optional_argument,      0,          opt_frames       },
   { "descriptors",     optional_argument,      0,          opt_descriptors  },
