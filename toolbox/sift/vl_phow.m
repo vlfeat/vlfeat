95d03b31dee96d8e9ba1dcae5c84be2feb934e16
Andrea Vedaldi <vedaldi@gmail.com>
2013-08-21 23:35:13 +0100

    vl_phow.m: improve doc style

--- toolbox/sift/vl_phow.m
+++ toolbox/sift/vl_phow.m
@@ -8,39 +8,39 @@ function [frames, descrs] = vl_phow(im, varargin)
 %   VL_PHOW() computes the gray-scale variant of the descriptor.  The
 %   COLOR option can be used to compute the color variant instead.
 %
-%   Verbose:: [false]
+%   Verbose:: false
 %     Set to true to turn on verbose output.
 %
-%   Sizes:: [[4 6 8 10]]
+%   Sizes:: [4 6 8 10]
 %     Scales at which the dense SIFT features are extracted. Each
 %     value is used as bin size for the VL_DSIFT() function.
 %
-%   Fast:: [true]
+%   Fast:: true
 %     Set to false to turn off the fast SIFT features computation by
 %     VL_DSIFT().
 %
-%   Step:: [2]
+%   Step:: 2
 %     Step (in pixels) of the grid at which the dense SIFT features
 %     are extracted.
 %
-%   Color:: [GRAY]
-%     Choose between GRAY (PHOW-gray), RGB, HSV, and OPPONENT
+%   Color:: 'gray'
+%     Choose between 'gray' (PHOW-gray), 'rgb', 'hsv', and 'opponent'
 %     (PHOW-color).
 %
-%   ContrastThreshold:: [0.005]
+%   ContrastThreshold:: 0.005
 %     Contrast threshold below which SIFT features are mapped to
 %     zero. The input image is scaled to have intensity range in [0,1]
 %     (rather than [0,255]) and this value is compared to the
 %     descriptor norm as returned by VL_DSIFT().
 %
-%   WindowSize:: [1.5]
+%   WindowSize:: 1.5
 %     Size of the Gaussian window in units of spatial bins.
 %
-%   Magnif:: [6]
+%   Magnif:: 6
 %     The image is smoothed by a Gaussian kernel of standard deviation
 %     SIZE / MAGNIF.
 %
-%   FloatDescriptors:: [false]
+%   FloatDescriptors:: false
 %     If set to TRUE, the descriptors are returned in floating point
 %     format.
 %
