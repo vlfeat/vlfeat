c6cd63afdbb91d627e4982fd17ee2cae58e44075
Andrea Vedaldi <vedaldi@gmail.com>
2013-09-01 15:34:18 +0100

    vl_fisher.c: doc: add two options

--- toolbox/fisher/vl_fisher.m
+++ toolbox/fisher/vl_fisher.m
@@ -1,14 +1,15 @@
 % VL_FISHER    Fisher vector feature encoding
-%   ENC = VL_FISHER(X, MEANS, COVARIANCES, PRIORS) computes the
-%   Fisher vector encoding of the vectors X relative to the Gaussian
-%   mixture model with means MEANS, covariances COVARIANCES, and pror
-%   mode probabilities PRIORS.
+%   ENC = VL_FISHER(X, MEANS, COVARIANCES, PRIORS) computes the Fisher
+%   vector encoding of the vectors X relative to the Gaussian mixture
+%   model with means MEANS, covariances COVARIANCES, and prior mode
+%   probabilities PRIORS.
 %
 %   X has one column per data vector (e.g. a SIFT descriptor), and
 %   MEANS and COVARIANCES one column per GMM component (covariance
-%   matrices are assumed diagonal). PRIORS has size equal to the
-%   number of GMM components. All data must be of the smae class,
-%   either SINGLE or DOUBLE.
+%   matrices are assumed diagonal, hence these are simply the variance
+%   of each data dimension). PRIORS has size equal to the number of
+%   GMM components. All data must be of the same class, either SINGLE
+%   or DOUBLE.
 %
 %   ENC is a vector of the same class of X of size equal to the
 %   product of the data dimension and the number of components.
@@ -22,6 +23,16 @@
 %   SquareRoot::
 %     If specified, the signed square root function is applied to
 %     ENC before normalization.
+%
+%   Improved::
+%     If specified, compute the improved variant of the Fisher
+%     Vector. This is equivalent to specifying the Normalized and
+%     SquareRoot options.
+%
+%   Fast::
+%     If specified, uses slightly less accurate computations but
+%     significantly increase the speed in some cases (particularly
+%     with a large number of Gaussian modes).
 %
 %   Verbose::
 %     Increase the verbosity level (may be specified multiple times).
