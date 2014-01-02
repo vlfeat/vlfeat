7afe6282cc5546ff0b58c24cf37626178cb508e6
Andrea Vedaldi <vedaldi@gmail.com>
2014-01-02 12:39:47 +0100

    vl_plotbox.m: improve MATLAB documentation

--- toolbox/plotop/vl_plotbox.m
+++ toolbox/plotop/vl_plotbox.m
@@ -1,21 +1,22 @@
 function [h, t] = vl_plotbox(boxes, varargin)
-% PLOTBOX  Plot boxes
-%   PLOTBOX(BOX) plots a box specified by the 4-dimensional vector
-%   BOXES = [XMIN YMIN XMAX YMAX]'. If BOXES is a 4 x N matrix, a box
-%   for each of the N columns is plotted.
+% VL_PLOTBOX  Plot boxes
+%   VL_PLOTBOX(BOX) plots a box specified by the 4-dimensional column
+%   vector BOXES = [XMIN YMIN XMAX YMAX]'. If BOXES is a 4 x N matrix,
+%   a box for each of the N columns is plotted.
 %
-%   H = PLOTBOX(BOXES) returns an handle to the line drawings
+%   H = VL_PLOTBOX(BOXES) returns a handle to the line drawing
 %   representing the boxes. For multiple boxes, H is a row vector with
 %   one handle per box.
 %
-%   PLOTBOX(BOXES, 'LABEL', LABEL) annotates the box with the string
-%   LABEL. If BOXES contains multiple boxes, then LABEL can be a cell
-%   array with one entry for each box. H is then a 2 x N array with
-%   handles to boxes and labels.
+%   VL_PLOTBOX(BOXES, 'LABEL', LABEL) annotates the box with the
+%   string LABEL. If BOXES contains multiple boxes, then LABEL can be
+%   a cell array with one entry for each box. H is then a 2 x N array
+%   with handles to boxes and corresponding labels.
 %
-%   PLOTBOX(BOXES, ...) passes any extra argument to the underlying
+%   VL_PLOTBOX(BOXES, ...) passes any extra argument to the underlying
 %   plotting function. The first optional argument can be a line
-%   specification string such as the one used by PLOT().
+%   specification string such as the one used by MATLAB's PLOT()
+%   function.
 %
 %   See also:: VL_PLOTFRAME().
 
