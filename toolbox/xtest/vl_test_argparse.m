2320d40db0d9ab5a985832fad367b8a65033594b
Karel Lenc <lenckarel@gmail.com>
2017-07-21 15:28:25 +0100

    Updated the vl_argparse.
    Added test cases for the new recursive behaviour (+'merge' and 'nonrecursive' options)

--- toolbox/xtest/vl_test_argparse.m
+++ toolbox/xtest/vl_test_argparse.m
@@ -25,6 +25,19 @@ try
 catch e
   return ;
 end
+% Test the recursive case
+opts.field1.field1 = 1 ;
+try
+  opts = vl_argparse(opts, {'field1.field2', 5}) ;
+catch e
+  return ;
+end
+% Test assigning non-struct to a struct
+try
+  opts = vl_argparse(opts, {'field1', 5}) ;
+catch e
+  return ;
+end
 assert(false) ;
 
 function test_leftovers()
@@ -37,3 +50,38 @@ opts2_.field2 = 2 ;
 opts2 = vl_argparse(opts2, args) ;
 
 assert(isequal(opts1,opts1_), isequal(opts2,opts2_)) ;
+
+
+function test_substruct()
+opts.field1.field1 = 1 ;
+opts.field2 = 2 ;
+
+opts_ = opts ;
+opts_.field1.field1 = 3 ;
+opts_.field2 = 10 ;
+
+% Test the dot-syntax
+opts = vl_argparse(opts, {'field2', 10, 'field1.field1', 3}) ;
+assert(isequal(opts, opts_)) ;
+
+% Test the struct arguments
+opts_.field1.field1 = 5;
+opts = vl_argparse(opts, {'field1', struct('field1', 5)}) ;
+assert(isequal(opts, opts_)) ;
+
+% Test the nonrecursive arg - replace a struct
+opts_.field1 = 3;
+opts = vl_argparse(opts, {'field1', 3}, 'nonrecursive') ;
+assert(isequal(opts, opts_)) ;
+
+
+function test_merge()
+opts.field1 = 1 ;
+opts.field2 = 2 ;
+
+opts_ = opts ;
+opts_.field1 = 3 ;
+opts_.field3 = 10 ;
+
+opts = vl_argparse(opts, {'field3', 10, 'field1', 3}, 'merge') ;
+assert(isequal(opts, opts_)) ;
